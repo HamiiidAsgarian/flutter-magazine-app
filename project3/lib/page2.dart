6a392ff7cdc8383ce607ed9f2e83e94e73ae6f75
hamiiid <Hamiiid.Asgarian@gmail.com>
2023-01-02 15:25:02 +0330

    Fixing page2 cards

--- project3/lib/page2.dart
+++ project3/lib/page2.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:project3/widgets.dart';
 
 class Page2 extends StatelessWidget {
   const Page2({super.key, required this.selectedItem});
@@ -13,229 +14,6 @@ class Page2 extends StatelessWidget {
   }
 }
 
-// class MyCarousel2 extends StatefulWidget {
-//   const MyCarousel2(
-//       {super.key,
-//       this.items = const [],
-//       this.currentIndex,
-//       this.onTap,
-//       this.leftOrRightResult});
-
-//   final List<Widget> items;
-//   final Function(MyCardsDragDirection)? leftOrRightResult;
-//   final Function(int)? currentIndex;
-//   final Function(CardsSelectionStatus)? onTap;
-
-//   @override
-//   State<MyCarousel2> createState() => _MyCarousel2State();
-// }
-
-// class _MyCarousel2State extends State<MyCarousel2>
-//     with TickerProviderStateMixin {
-//   late AnimationController _rotateAnimCntrl;
-
-//   late Animation<double> _rotateAnim;
-//   late Animation<double> _translateXAnim;
-//   late Animation<double> _scaleAnim;
-//   late Animation<double> _scaleAnim2;
-
-//   late List<Animation<double>> _rotateAnims;
-
-//   late List<Widget> _items;
-
-//   late List<Widget> mywid;
-//   @override
-//   void initState() {
-//     _rotateAnimCntrl = AnimationController(
-//         vsync: this, duration: const Duration(milliseconds: 5000));
-
-//     _rotateAnim = Tween<double>(begin: 0, end: pi).animate(
-//         CurvedAnimation(parent: _rotateAnimCntrl, curve: Curves.linear));
-
-//     _scaleAnim = Tween<double>(begin: 1.0, end: 0.7).animate(
-//         CurvedAnimation(parent: _rotateAnimCntrl, curve: Curves.linear));
-//     _scaleAnim2 = Tween<double>(begin: .7, end: 1.0).animate(
-//         CurvedAnimation(parent: _rotateAnimCntrl, curve: Curves.linear));
-
-//     _translateXAnim = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
-//         parent: _rotateAnimCntrl,
-//         curve: const Interval(0.0, 1.0, curve: Curves.linear)));
-
-//     _rotateAnims = [_rotateAnim];
-
-//     _items = widget.items;
-
-//     mywid = [
-//       GestureDetector(
-//         onTapUp: (details) {
-//           rotate();
-//         },
-//         child: AnimatedBuilder(
-//           animation: _rotateAnim,
-//           builder: (context, child) {
-//             // double x = _translateXAnimGo.isCompleted
-//             //     ? _translateXAnimBack.value
-//             //     : _translateXAnimGo.value;
-//             double x = _translateXAnim.value;
-//             double res = (-4 * (pow(x, 2)) + (x * 4));
-
-//             print(res);
-//             return Transform(
-//                 alignment: Alignment.center,
-//                 // origin: const Offset(-20, 0),
-//                 transform: Matrix4(
-//                   _scaleAnim.value,
-//                   0,
-//                   0,
-//                   0,
-//                   0,
-//                   _scaleAnim.value,
-//                   0,
-//                   0,
-//                   0,
-//                   0,
-//                   1,
-//                   0.01,
-//                   res * 30,
-//                   0,
-//                   0,
-//                   1,
-//                 )..rotateY(-_rotateAnim.value),
-
-//                 // transform: Matrix4.identity()
-//                 //   ..setEntry(3, 2,
-//                 //       0.003) // col = 2, row = 3 & 0.003 = depth perception in the Z direction
-//                 //   ..rotateY(_rotateAnim.value),
-//                 child: Container(
-//                     width: _cardWith,
-//                     height: _cardHeight,
-//                     color: Colors.greenAccent));
-//           },
-//         ),
-//         onTap: () {},
-//       ),
-//       GestureDetector(
-//         onTapUp: (details) {
-//           rotate();
-//         },
-//         child: AnimatedBuilder(
-//           animation: _rotateAnim,
-//           builder: (context, child) {
-//             // double x = _translateXAnimGo.isCompleted
-//             //     ? _translateXAnimBack.value
-//             //     : _translateXAnimGo.value;
-//             double x = _translateXAnim.value;
-//             double res = (-4 * (pow(x, 2)) + (x * 4));
-
-//             if (_rotateAnim.value > pi / 2) {
-//               testColor = Colors.brown;
-//             } else {
-//               testColor = Colors.yellow;
-
-//               print("* ${mywid.length}");
-//               if (mywid.length >= 2) {
-//                 mywid.removeAt(1);
-//               }
-//               print("*% ${mywid.length}");
-//             }
-//             ;
-//             return Transform(
-//                 alignment: Alignment.center,
-//                 // origin: const Offset(-20, 0),
-//                 transform: Matrix4(
-//                   _scaleAnim2.value,
-//                   0,
-//                   0,
-//                   0,
-//                   0,
-//                   _scaleAnim2.value,
-//                   0,
-//                   0,
-//                   0,
-//                   0,
-//                   1,
-//                   0.01,
-//                   -(res * 30),
-//                   0,
-//                   0,
-//                   1,
-//                 )..rotateY(-_rotateAnim.value),
-
-//                 // transform: Matrix4.identity()
-//                 //   ..setEntry(3, 2,
-//                 //       0.003) // col = 2, row = 3 & 0.003 = depth perception in the Z direction
-//                 //   ..rotateY(_rotateAnim.value),
-//                 child: Container(
-//                     width: _cardWith, height: _cardHeight, color: testColor));
-//           },
-//         ),
-//         onTap: () {},
-//       ),
-//     ];
-
-//     super.initState();
-//   }
-
-//   final double _cardWith = 100;
-//   final double _cardHeight = 100;
-
-//   Color testColor = Colors.yellow;
-
-//   @override
-//   void dispose() {
-//     _rotateAnimCntrl.dispose();
-//     // _returnToPositionCntrl.dispose();
-
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     print("--- ${mywid.length}");
-//     _rotateAnimCntrl.value == pi ? print("object") : null;
-//     print("object");
-//     return Column(
-//       mainAxisAlignment: MainAxisAlignment.end,
-//       crossAxisAlignment: CrossAxisAlignment.stretch,
-//       children: [
-//         FloatingActionButton(onPressed: () {
-//           _rotateAnimCntrl.reverse();
-//         }),
-//         Expanded(
-//           child: Center(
-//             child: Stack(
-//               children: _cardsGEnerator(),
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-
-//   List<Widget> _cardsGEnerator() {
-//     List<Widget> temp = [];
-
-//     for (var i = 0; i < mywid.length; i++) {}
-
-//     return mywid
-//         .map((e) => GestureDetector(
-//               onTap: () {
-//                 setState(() {
-//                   mywid.removeLast();
-//                 });
-//               },
-//               child: Center(
-//                   child: FlutterLogo(size: (Random().nextInt(200).toDouble()))),
-//             ))
-//         .toList();
-//   }
-
-//   rotate() async {
-//     await _rotateAnimCntrl.forward();
-//     // await _rotateAnimCntrl.reverse();
-//   }
-// }
-
 class MyCarousel2 extends StatefulWidget {
   const MyCarousel2({super.key});
 
@@ -247,7 +25,7 @@ List<Widget> _items = [const FlutterLogo(), const FlutterLogo()];
 
 class _MyCarousel2State extends State<MyCarousel2>
     with TickerProviderStateMixin {
-  late AnimationController _rotateAnimCntrl;
+  late AnimationController _mainAnimCntrl;
 
   late Animation<double> _rotateAnim1;
   late Animation<double> _rotateAnim2;
@@ -255,40 +33,59 @@ class _MyCarousel2State extends State<MyCarousel2>
   late Animation<double> _translateXAnim;
   late Animation<double> _translateXAnim2;
 
+  static const double _cardsTravelDestence = 30;
+
   @override
   void initState() {
-    _rotateAnimCntrl = AnimationController(
+    _mainAnimCntrl = AnimationController(
         vsync: this, duration: const Duration(milliseconds: 600));
 
     _rotateAnim1 = Tween<double>(begin: 0.0, end: pi / 2).animate(
         CurvedAnimation(
-            parent: _rotateAnimCntrl, curve: const Interval(0.0, .5)));
+            parent: _mainAnimCntrl, curve: const Interval(0.0, .5)));
     _rotateAnim2 = Tween<double>(begin: pi / 2, end: 0.0).animate(
         CurvedAnimation(
-            parent: _rotateAnimCntrl, curve: const Interval(0.5, 1.0)));
+            parent: _mainAnimCntrl, curve: const Interval(0.5, 1.0)));
 
-    _translateXAnim = Tween<double>(begin: 0.0, end: 30.0).animate(
-        CurvedAnimation(
-            parent: _rotateAnimCntrl, curve: const Interval(0.0, .5)));
-    _translateXAnim2 = Tween<double>(begin: 30.0, end: 0.0).animate(
-        CurvedAnimation(
-            parent: _rotateAnimCntrl, curve: const Interval(0.5, 1.0)));
+    _translateXAnim = Tween<double>(begin: 0.0, end: _cardsTravelDestence)
+        .animate(CurvedAnimation(
+            parent: _mainAnimCntrl, curve: const Interval(0.0, .5)));
+    _translateXAnim2 = Tween<double>(begin: _cardsTravelDestence, end: 0.0)
+        .animate(CurvedAnimation(
+            parent: _mainAnimCntrl, curve: const Interval(0.5, 1.0)));
 
     super.initState();
   }
 
+  @override
+  void dispose() {
+    _mainAnimCntrl.dispose();
+    super.dispose();
+  }
+
+  MyCardsDragDirection _dragDirection = MyCardsDragDirection.right;
   List<Widget> rawItems = [
     Container(
-      width: 100,
-      height: 100,
+      width: 400,
+      height: 400,
       color: Colors.pinkAccent,
       child: const Text("1"),
     ),
     Container(
-        width: 100,
-        height: 100,
+        width: 400,
+        height: 400,
         color: Colors.lightGreenAccent,
         child: const Text("2")),
+    // Container(
+    //     width: 400,
+    //     height: 400,
+    //     color: Colors.blueAccent,
+    //     child: const Text("2")),
+    // Container(
+    //     width: 400,
+    //     height: 400,
+    //     color: Colors.yellowAccent,
+    //     child: const Text("2")),
   ];
 
   @override
@@ -296,15 +93,66 @@ class _MyCarousel2State extends State<MyCarousel2>
     List x = _carsGenerator();
     return Column(
       children: [
-        Align(
-          alignment: Alignment.topCenter,
-          child: FloatingActionButton(onPressed: () async {
-            await _rotateAnimCntrl.forward();
-            _rotateAnimCntrl.reset();
-            setState(() {
-              rawItems = rawItems.reversed.toList();
-            });
-          }),
+        Center(
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            FloatingActionButton(onPressed: () async {
+              _rotateAnim1 = Tween<double>(begin: 0.0, end: pi / 2).animate(
+                  CurvedAnimation(
+                      parent: _mainAnimCntrl, curve: const Interval(0.0, .5)));
+              _rotateAnim2 = Tween<double>(begin: pi / 2, end: 0.0).animate(
+                  CurvedAnimation(
+                      parent: _mainAnimCntrl, curve: const Interval(0.5, 1.0)));
+
+              _translateXAnim = Tween<double>(
+                      begin: 0.0, end: _cardsTravelDestence)
+                  .animate(CurvedAnimation(
+                      parent: _mainAnimCntrl,
+                      curve: const Interval(0.0, .5, curve: Curves.easeOut)));
+              _translateXAnim2 = Tween<double>(
+                      begin: _cardsTravelDestence, end: 0.0)
+                  .animate(CurvedAnimation(
+                      parent: _mainAnimCntrl,
+                      curve: const Interval(0.5, 1.0, curve: Curves.easeIn)));
+
+              setState(() {
+                _dragDirection = MyCardsDragDirection.right;
+              });
+
+              await _mainAnimCntrl.forward();
+              _mainAnimCntrl.reset();
+              setState(() {
+                rawItems = rawItems.reversed.toList();
+              });
+            }),
+            FloatingActionButton(onPressed: () async {
+              _rotateAnim1 = Tween<double>(begin: 0.0, end: -pi / 2).animate(
+                  CurvedAnimation(
+                      parent: _mainAnimCntrl, curve: const Interval(0.0, .5)));
+              _rotateAnim2 = Tween<double>(begin: -pi / 2, end: 0.0).animate(
+                  CurvedAnimation(
+                      parent: _mainAnimCntrl, curve: const Interval(0.5, 1.0)));
+              _translateXAnim = Tween<double>(
+                      begin: 0.0, end: -_cardsTravelDestence)
+                  .animate(CurvedAnimation(
+                      parent: _mainAnimCntrl,
+                      curve: const Interval(0.0, .5, curve: Curves.easeOut)));
+              _translateXAnim2 = Tween<double>(
+                      begin: -_cardsTravelDestence, end: 0.0)
+                  .animate(CurvedAnimation(
+                      parent: _mainAnimCntrl,
+                      curve: const Interval(0.5, 1.0, curve: Curves.easeIn)));
+
+              setState(() {
+                _dragDirection = MyCardsDragDirection.left;
+              });
+
+              await _mainAnimCntrl.forward();
+              _mainAnimCntrl.reset();
+              setState(() {
+                rawItems = rawItems.reversed.toList();
+              });
+            }),
+          ]),
         ),
         Center(child: Stack(children: [...x])),
       ],
@@ -312,18 +160,21 @@ class _MyCarousel2State extends State<MyCarousel2>
   }
 
   List<Widget> _carsGenerator() {
-    print("object");
-    // bool firstOnTop = true;
-    // bool hasReachedToActivationLimit = false;
     List<Widget> res = [
       AnimatedBuilder(
-        animation: _rotateAnimCntrl,
+        animation: _mainAnimCntrl,
         builder: ((context, child) {
-          print(_rotateAnim2.value);
+          print(_dragDirection);
+          bool myConditionAnim1 = _dragDirection == MyCardsDragDirection.right
+              ? _rotateAnim1.value == pi / 2
+              : _rotateAnim1.value == -(pi / 2);
+          bool myConditionAnim2 = _dragDirection == MyCardsDragDirection.right
+              ? _rotateAnim2.value == (pi / 2)
+              : _rotateAnim2.value == -(pi / 2);
           return Stack(
             children: [
               Visibility(
-                visible: !(_rotateAnim1.value == pi / 2),
+                visible: !(myConditionAnim1),
                 child: Transform(
                     alignment: Alignment.center,
                     transform: Matrix4(
@@ -338,7 +189,7 @@ class _MyCarousel2State extends State<MyCarousel2>
                       0,
                       0,
                       1,
-                      0.003,
+                      0.0015,
                       _translateXAnim.value,
                       0,
                       0,
@@ -347,7 +198,7 @@ class _MyCarousel2State extends State<MyCarousel2>
                     child: rawItems[0]),
               ),
               Visibility(
-                visible: !(_rotateAnim1.value == pi / 2),
+                visible: !(myConditionAnim1),
                 child: Transform(
                     alignment: Alignment.center,
                     transform: Matrix4(
@@ -362,7 +213,7 @@ class _MyCarousel2State extends State<MyCarousel2>
                       0,
                       0,
                       1,
-                      0.003,
+                      0.0015,
                       -_translateXAnim.value,
                       0,
                       0,
@@ -371,7 +222,7 @@ class _MyCarousel2State extends State<MyCarousel2>
                     child: rawItems[1]),
               ),
               Visibility(
-                visible: !(_rotateAnim2.value == (pi / 2)),
+                visible: !(myConditionAnim2),
                 child: Transform(
                     alignment: Alignment.center,
                     transform: Matrix4(
@@ -386,7 +237,7 @@ class _MyCarousel2State extends State<MyCarousel2>
                       0,
                       0,
                       1,
-                      0.003,
+                      0.0015,
                       -_translateXAnim2.value,
                       0,
                       0,
@@ -395,7 +246,7 @@ class _MyCarousel2State extends State<MyCarousel2>
                     child: rawItems[1]),
               ),
               Visibility(
-                visible: !(_rotateAnim2.value == (pi / 2)),
+                visible: !(myConditionAnim2),
                 child: Transform(
                     alignment: Alignment.center,
                     transform: Matrix4(
@@ -410,7 +261,7 @@ class _MyCarousel2State extends State<MyCarousel2>
                       0,
                       0,
                       1,
-                      0.003,
+                      0.0015,
                       _translateXAnim2.value,
                       0,
                       0,
